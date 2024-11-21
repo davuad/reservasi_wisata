f8435eccf76f910319d3841c4434c5bb51ce7824
davuad <davuandrias01@gmail.com>
2024-11-21 20:48:47 +0700

    Memperbaiki table reservation dan menambah dropdown

--- app/views/template/footer.php
+++ app/views/template/footer.php
@@ -1,9 +1,4 @@
 <!-- app/views/template/footer.php -->
-    <!-- Footer -->
-    <footer class="bg-dark text-white text-center py-4">
-        <p>&copy; 2024 Reservasi Wisata. All rights reserved.</p>
-    </footer>
-
     <!-- Menambahkan Bootstrap JS -->
     <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.3.0/dist/js/bootstrap.bundle.min.js"></script>
 </body>
--- app/views/user/edit.php
+++ app/views/user/edit.php
@@ -3,35 +3,38 @@
 require_once __DIR__ . '/../template/header.php';
 ?>
 
-<div class="container mt-5">
+<div class="container mt-3 mb-3">
     <!-- Card untuk Form Edit -->
     <div class="row justify-content-center">
-    <div class="col-md-8">
-    <div class="card">
-        <div class="card-header bg-secondary text-white text-center">
-            <h3>Edit User</h3>
-        </div>
-        <div class="card-body">
-            <!-- Form untuk update data pengguna -->
-            <form action="/user/update/<?php echo $user['id_users']; ?>" method="POST">
-                <div class="mb-3">
-                    <label for="nama" class="form-label">Nama:</label>
-                    <input type="text" id="nama" name="nama" class="form-control" value="<?php echo $user['nama']; ?>" required>
+        <div class="col-md-8">
+            <div class="card">
+                <div class="card-header bg-secondary text-white text-center">
+                    <h3>Edit User</h3>
                 </div>
+                <div class="card-body">
+                    <!-- Form untuk update data pengguna -->
+                    <form action="/user/update/<?php echo $user['id_users']; ?>" method="POST">
+                        <div class="mb-3">
+                            <label for="nama" class="form-label">Nama:</label>
+                            <input type="text" id="nama" name="nama" class="form-control" value="<?php echo $user['nama']; ?>" required>
+                        </div>
 
-                <div class="mb-3">
-                    <label for="email" class="form-label">Email:</label>
-                    <input type="email" id="email" name="email" class="form-control" value="<?php echo $user['email']; ?>" required>
+                        <div class="mb-3">
+                            <label for="email" class="form-label">Email:</label>
+                            <input type="email" id="email" name="email" class="form-control" value="<?php echo $user['email']; ?>" required>
+                        </div>
+
+                        <div class="mb-3">
+                            <label for="nomor_telepon" class="form-label">Nomor Telepon:</label>
+                            <input type="text" id="nomor_telepon" name="nomor_telepon" class="form-control" value="<?php echo $user['nomor_telepon']; ?>" required>
+                        </div>
+                        <div>
+                            <button type="submit" class="btn btn-success">Update</button>
+                            <a href="/user/index" class="btn btn-secondary">Back to List</a>
+                        </div>
+                    </form>
                 </div>
-
-                <div class="mb-3">
-                    <label for="nomor_telepon" class="form-label">Nomor Telepon:</label>
-                    <input type="text" id="nomor_telepon" name="nomor_telepon" class="form-control" value="<?php echo $user['nomor_telepon']; ?>" required>
-                </div>
-
-                <button type="submit" class="btn btn-success">Update</button>
-                <a href="/user/index" class="btn btn-secondary">Back to List</a>
-            </form>
+            </div>
         </div>
     </div>
 </div>
