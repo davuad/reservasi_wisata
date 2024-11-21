a4ee093e90bba68f421c1a5672bef3351c91f6ae
davuad <davuandrias01@gmail.com>
2024-11-21 13:44:38 +0700

    Update routes.php
    routes final

--- routes.php
+++ routes.php
@@ -1,30 +1,68 @@
 <?php
-require_once 'app/controllers/ReservationController.php';
+// Routing untuk Halaman Utama
+require_once 'app/controllers/HomeController.php';
 
-$controller = new ReservationController();
+// Routing 
+require_once 'app/controllers/ReservationController.php';
+require_once 'app/controllers/DestinationsController.php';
+require_once 'app/controllers/UserController.php';
+
+$userController = new UserController();
+$destinasiController = new DestinasiController();
+$reservasiController = new ReservationController();
+
 $url = $_SERVER['REQUEST_URI'];
 $requestMethod = $_SERVER['REQUEST_METHOD'];
 
-// Menghapus bagian prefix dari URL
-$url = str_replace('/P_Web2_s3/reservasi_wisata/', '', $url);
-
-if ($url == '/reservasi/index' || $url == '/') {
-    $controller->index();
+// Menampilkan halaman utama / home
+if ($url == '/' || $url == '/') {
+    new HomeController();
+}elseif ($url == '/user/index' || $url == '/user') {
+    $userController->index();
+} elseif ($url == '/user/create' && $requestMethod == 'GET') {
+    $userController->create();
+} elseif ($url == '/user/store' && $requestMethod == 'POST') {
+    $userController->store();
+} elseif (preg_match('/\/user\/edit\/(\d+)/', $url, $matches) && $requestMethod == 'GET') {
+    $userId = $matches[1];
+    $userController->edit($userId);
+} elseif (preg_match('/\/user\/update\/(\d+)/', $url, $matches) && $requestMethod == 'POST') {
+    $userId = $matches[1];
+    $userController->update($userId, $_POST);
+} elseif (preg_match('/\/user\/delete\/(\d+)/', $url, $matches) && $requestMethod == 'GET') {
+    $userId = $matches[1];
+    $userController->delete($userId);
+}elseif ($url == '/destinasi/index' || $url == '/destinasi') {
+    $destinasiController->index();
+} elseif ($url == '/destinasi/create' && $requestMethod == 'GET') {
+    $destinasiController->create();
+} elseif ($url == '/destinasi/store' && $requestMethod == 'POST') {
+    $destinasiController->store();
+} elseif (preg_match('/\/destinasi\/edit\/(\d+)/', $url, $matches) && $requestMethod == 'GET') {
+    $destinasiId = $matches[1];
+    $destinasiController->edit($destinasiId);
+} elseif (preg_match('/\/destinasi\/update\/(\d+)/', $url, $matches) && $requestMethod == 'POST') {
+    $destinasiId = $matches[1];
+    $destinasiController->update($destinasiId, $_POST);
+} elseif (preg_match('/\/destinasi\/delete\/(\d+)/', $url, $matches) && $requestMethod == 'GET') {
+    $destinasiId = $matches[1];
+    $destinasiController->delete($destinasiId);
+}elseif ($url == '/reservasi/index' || $url == '/reservasi') {
+    $reservasiController->index();
 } elseif ($url == '/reservasi/create' && $requestMethod == 'GET') {
-    $controller->create();
+    $reservasiController->create();
 } elseif ($url == '/reservasi/store' && $requestMethod == 'POST') {
-    $controller->store();
+    $reservasiController->store();
 } elseif (preg_match('/\/reservasi\/edit\/(\d+)/', $url, $matches) && $requestMethod == 'GET') {
     $reservation_id = $matches[1];
-    $controller->edit($reservation_id);
+    $reservasiController->edit($reservation_id);
 } elseif (preg_match('/\/reservasi\/update\/(\d+)/', $url, $matches) && $requestMethod == 'POST') {
     $reservation_id = $matches[1];
-    $controller->update($reservation_id);
+    $reservasiController->update($reservation_id);
 } elseif (preg_match('/\/reservasi\/delete\/(\d+)/', $url, $matches) && $requestMethod == 'GET') {
     $reservation_id = $matches[1];
-    $controller->delete($reservation_id);
+    $reservasiController->delete($reservation_id);
 } else {
     http_response_code(404);
     echo "404 Not Found";
 }
-?>
